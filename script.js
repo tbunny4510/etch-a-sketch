f9ae5f617fa173ca6e6b5fad2fabea227ff36304
Sean Walters <tbunny4510@gmail.com>
2023-02-21 22:42:41 -0500

    Create function to make x-amt of boxes

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 const container = document.querySelector('#container');
 const clear = document.querySelector('.clear');
+const amtButton = document.querySelector('.amt');
 
-for(let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('containers');
-    for(let j = 0; j < 16; j++) {
-        const divRow = document.createElement('div');
-        divRow.classList.add('rows');
-        div.appendChild(divRow);
+function createBoxes(num) {
+    for(let i = 0; i < num; i++) {
+        const div = document.createElement('div');
+        div.classList.add('containers');
+        for(let j = 0; j < num; j++) {
+            const divRow = document.createElement('div');
+            divRow.classList.add('rows');
+            div.appendChild(divRow);
+        }
+        container.appendChild(div);
     }
-    container.appendChild(div);
-}
+    hover();
+};
 
-const divHover = document.querySelectorAll('.rows');
+function hover() {
+    const divHover = document.querySelectorAll('.rows');
 
-divHover.forEach((box) => {
-    box.addEventListener('mouseover', () => {
-        box.classList.add('rowsHover');
-    });
-});
-
-clear.addEventListener('click', () => {
     divHover.forEach((box) => {
-        box.classList.remove('rowsHover');
-    })
+        box.addEventListener('mouseover', () => {
+            box.classList.add('rowsHover');
+        });
+        clear.addEventListener('click', () => {
+            box.classList.remove('rowsHover');
+        });
+    });
+};
+
+amtButton.addEventListener('click', () => {
+    let num = prompt("How many boxes?");
+    removeBoxes();
+    createBoxes(num);
 });
